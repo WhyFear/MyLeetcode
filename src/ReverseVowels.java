b35fdcaa81a0aeab33943f44b2a3159e6196781a
WhyFear <893843891@qq.com>
2020-08-07 21:20:07 +0800

    new

--- src/ReverseVowels.java
+++ src/ReverseVowels.java
@@ -1,12 +1,16 @@
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * https://leetcode-cn.com/problems/reverse-vowels-of-a-string/
+ * 好蠢的写法
+ */
 public class ReverseVowels {
     public String reverseVowels(String s) {
         int len = s.length();
+        if (len < 2) return s;
         int i = 0, j = len - 1;
-        StringBuilder front = new StringBuilder();
-        StringBuilder tail = new StringBuilder();
+        char[] chars = s.toCharArray();
 
         Set<Character> set = new HashSet<>() {
             {
@@ -18,19 +22,58 @@ public class ReverseVowels {
             }
         };
         while (i < j) {
-            while (i < j && !set.contains(s.charAt(i))) {
+            if (set.contains(Character.toLowerCase(chars[i])) && set.contains(Character.toLowerCase(chars[j]))) {
+                //swap
+                char temp = chars[i];
+                chars[i] = chars[j];
+                chars[j] = temp;
+                i++;
+                j--;
+            } else if (set.contains(Character.toLowerCase(chars[i]))) {
+                j--;
+            } else if (set.contains(Character.toLowerCase(chars[j]))) {
+                i++;
+            } else {
+                i++;
+                j--;
+            }
+        }
+        return String.valueOf(chars);
+    }
+}
+/*
+    public String reverseVowels(String s) {
+        int len = s.length();
+        if (len < 2) return s;
+        int i = 0, j = len - 1;
+        StringBuilder front = new StringBuilder();
+        StringBuilder tail = new StringBuilder();
+
+        Set<String> set = new HashSet<>() {
+            {
+                add("a");
+                add("e");
+                add("i");
+                add("o");
+                add("u");
+            }
+        };
+        while (i <= j) {
+            while (i < j && !set.contains(String.valueOf(s.charAt(i)).toLowerCase())) {
                 front.append(s.charAt(i));
                 i++;
             }
-            while (j > i && !set.contains(s.charAt(j))) {
+            while (j > i && !set.contains(String.valueOf(s.charAt(j)).toLowerCase())) {
                 tail.append(s.charAt(j));
                 j--;
             }
-            front.append(s.charAt(j));
-            tail.append(s.charAt(i));
+            if (i < j) {
+                front.append(s.charAt(j));
+                tail.append(s.charAt(i));
+            } else if (i == j) front.append(s.charAt(j));
             i++;
             j--;
         }
         return front.append(tail.reverse()).toString();
     }
-}
+ */

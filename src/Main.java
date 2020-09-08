fdedcd64b04bd3a45de4c8210a934b145e41ff4c
WhyFear <893843891@qq.com>
2020-09-08 11:08:35 +0800

    new

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
 
-        LevelOrderBottom fuck = new LevelOrderBottom();
+        Combine fuck = new Combine();
         int[] temp = new int[]{1, 5, 233, 7};
         int[][] temp1 = new int[][]{{1, 3}, {3, 0, 1}, {2}, {0}};
         char[] tasks = new char[]{'A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H', 'I', 'I', 'J', 'J', 'K', 'K', 'L', 'L', 'M', 'M', 'N', 'N', 'O', 'O', 'P', 'P', 'Q', 'Q', 'R', 'R', 'S', 'S', 'T', 'T', 'U', 'U', 'V', 'V', 'W', 'W', 'X', 'X', 'Y', 'Y', 'Z', 'Z'};
@@ -27,7 +27,7 @@ public class Main {
 //            p.next = new ListNode(num);
 //            p = p.next;
 //        }
-        System.out.println(fuck.levelOrderBottom(root));
+        System.out.println(fuck.combine(0,0));
 //        fuck.recoverTree(root);
 
         long endTime = System.currentTimeMillis();    //获取结束时间

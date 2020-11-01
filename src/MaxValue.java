ea9163a8f9ea925a8db5b690c6a8f2e65875002c
WhyFear <893843891@qq.com>
2020-11-01 14:46:22 +0800

    new

---
+++ src/MaxValue.java
@@ -0,0 +1,25 @@
+/**
+ * https://leetcode-cn.com/problems/li-wu-de-zui-da-jie-zhi-lcof/
+ * 可以原地修改
+ */
+public class MaxValue {
+    public int maxValue(int[][] grid) {
+        int row = grid.length;
+        if (row == 0) return 0;
+        int column = grid[0].length;
+        int[][] dp = new int[row][column];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < row; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+        for (int i = 1; i < column; i++) {
+            dp[0][i] = dp[0][i - 1] + grid[0][i];
+        }
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < column; j++) {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        return dp[row - 1][column - 1];
+    }
+}

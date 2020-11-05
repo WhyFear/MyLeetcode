b913235ecad65cf7a14c48dfd45c504a26da161b
ZengLH <893843891@qq.com>
2020-11-05 15:31:19 +0800

    new

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,8 @@ public class Main {
         int[][] temp1 = new int[][]{{1, 2}, {3, 4}, {1, 4}, {2, 3}};
         char[] tasks = new char[]{'A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H', 'I', 'I', 'J', 'J', 'K', 'K', 'L', 'L', 'M', 'M', 'N', 'N', 'O', 'O', 'P', 'P', 'Q', 'Q', 'R', 'R', 'S', 'S', 'T', 'T', 'U', 'U', 'V', 'V', 'W', 'W', 'X', 'X', 'Y', 'Y', 'Z', 'Z'};
         char[][] board = new char[][]{{'5', '3', '.', '.', '7', '.', '.', '.', '.'}, {'6', '.', '.', '1', '9', '5', '.', '.', '.'}, {'.', '9', '8', '.', '.', '.', '.', '6', '.'}, {'8', '.', '.', '.', '6', '.', '.', '.', '3'}, {'4', '.', '.', '8', '.', '3', '.', '.', '1'}, {'7', '.', '.', '.', '2', '.', '.', '.', '6'}, {'.', '6', '.', '.', '.', '.', '2', '8', '.'}, {'.', '.', '.', '4', '1', '9', '.', '.', '5'}, {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
-        String[] A = new String[]{"word","good","best","good"};
-//        List<String> listString = new ArrayList<>(Arrays.asList(A));
+        String[] A = new String[]{"hot", "dot", "dog", "lot", "log", "log"};
+        List<String> listString = new ArrayList<>(Arrays.asList(A));
 //        List<List<Integer>> rooms = new ArrayList<>();
 //        TreeNode root = new TreeNode(1);
 //        root.left = new TreeNode(3);
@@ -27,10 +27,10 @@ public class Main {
 //            p = p.next;
 //        }
 
-        FindSubstring fuck = new FindSubstring();
+        LadderLength fuck = new LadderLength();
 //        fuck.solveSudoku(board);
         Scanner in = new Scanner(System.in);
-        System.out.println(fuck.findSubstring("wordgoodgoodgoodbestword", A));
+        System.out.println(fuck.ladderLength("hit", "cog", listString));
 //        System.out.println(Arrays.toString(fuck.permutation("aab")));
 
         long endTime = System.currentTimeMillis();    //获取结束时间

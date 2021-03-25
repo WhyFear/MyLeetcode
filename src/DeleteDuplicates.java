4e1bbc1523c5f883a7bc9045fe3317d867ae6441
WhyFear <893843891@qq.com>
2021-03-25 15:54:04 +0800

    每日一题

--- src/DeleteDuplicates.java
+++ src/DeleteDuplicates.java
@@ -1,7 +1,9 @@
-/**
- * https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list/
- */
+import javax.swing.text.TabExpander;
+
 public class DeleteDuplicates {
+    /**
+     * https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list/
+     */
     public ListNode deleteDuplicates(ListNode head) {
         ListNode p = head;
         while (p != null) {
@@ -11,5 +13,31 @@ public class DeleteDuplicates {
             p = p.next;
         }
         return head;
+    }
+
+    /**
+     * https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list-ii/
+     */
+    public ListNode deleteDuplicatesii(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode temp = new ListNode(head.val - 1); //应对头几个节点就是相同的情况。
+        temp.next = head;
+        ListNode p = temp;
+        ListNode pre = temp;
+        boolean flag = false;
+        while (p != null) {
+            while (p.next != null && p.next.val == p.val) { //相同的就全部跳过
+                p.next = p.next.next;
+                flag = true;
+            }
+            if (flag) {
+                pre.next = p.next; //从相同的节点前一个开始，直接连接到后续不相同的第一个节点
+                flag = false;
+                p = pre; //p重置到它前面
+            }
+            pre = p;
+            p = p.next;
+        }
+        return temp.next;
     }
 }

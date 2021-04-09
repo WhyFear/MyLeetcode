012e03ffe96d91313fc3fbc0313e7026a34ff70e
WhyFear <893843891@qq.com>
2021-04-09 13:36:57 +0800

    每日一题 垃圾代码

--- src/FindMin.java
+++ src/FindMin.java
@@ -1,7 +1,7 @@
-/**
- * https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array/
- */
 public class FindMin {
+    /**
+     * https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array/
+     */
     public int findMin(int[] nums) {
         int min = Integer.MAX_VALUE;
         for (int num : nums) {
@@ -9,4 +9,24 @@ public class FindMin {
         }
         return min;
     }
+
+    /**
+     * https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array-ii/
+     * chaoxi
+     */
+    public int findMinII(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = (right + left) / 2;
+            if (nums[mid] > nums[right])
+                left = mid + 1;
+            else if (nums[mid] < nums[right])
+                right = mid;
+            else
+                right--;
+        }
+        return nums[left];
+    }
+
+
 }
